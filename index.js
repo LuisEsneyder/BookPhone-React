d3d9c6a772b93fcb9197c318aabc37f8b4fef0e2
Luis Fernández <luisexneider1999@gmail.com>
2022-03-10 23:03:54 -0500

    Puerto

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.post('/api/persons',(req,res)=>{
     persons = persons.concat(persona)
     res.json(persona)
 })
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT,()=>{
     console.log(`server on in the port ${PORT}`)
